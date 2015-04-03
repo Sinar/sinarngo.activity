e679e420685643cb432a0c3b4a01ca6ce45460f1
Khairil Yusof <khairil.yusof@gmail.com>
2015-04-03 19:23:55 +0800

    Add activity content type.

---
+++ sinarngo/activity/content/activity.py
@@ -0,0 +1,47 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from plone.autoform.interfaces import IFormFieldProvider
+from zope.interface import alsoProvides
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.multilingualbehavior.directives import languageindependent
+from collective import dexteritytextindexer
+
+from sinarngo.activity import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class IActivity(form.Schema, IImageScaleTraversable):
+    """
+    NGO Activity
+    """
+
+    dexteritytextindexer.searchable('title')
+    title = schema.TextLine(title=u'Title',
+                            description=u'Title of Campaign.')
+
+    dexteritytextindexer.searchable('description')
+    description = schema.Text(title=u'Description',
+                              description=u'Brief description '
+                              'or summary of campaign.')
+
+    dexteritytextindexer.searchable('body')
+    body = RichText(title=u'Details of Activity')
+
+alsoProvides(IActivity, IFormFieldProvider)
